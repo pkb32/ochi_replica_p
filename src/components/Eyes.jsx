ee059550655c683159f144d0ba2ac283648a0c8b
pkb32 <113055313+pkb32@users.noreply.github.com>
2025-02-01 17:22:47 +0530

    eyes edit

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -28,7 +28,7 @@ const [rotate, setRotate] =useState(0);
                     <div className='relative  w-2/3 h-2/3 rounded-full bg-zinc-900'>
                         <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10'>
                             <div className=' w-10 h-10 rounded-full bg-zinc-100'>
-                            </div>
+                        </div>
                             
                         </div>
                         <p className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>Play</p>
