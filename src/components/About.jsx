5b3adf117a092ce4b17dd0c99399c321b357748f
pkb32 <113055313+pkb32@users.noreply.github.com>
2025-02-01 13:36:40 +0530

    about section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,10 +21,10 @@ function About() {
             <p className='pt-12'> We believe the mix of strategy and design (with a bit of coffee) is what makes your message clear, convincing, and captivating.</p>
             </div>
 
-            <div className='pt-32 ml-[10vw] font-["Neue_Montreal"] font-medium text-[1.1vw] '>
+            <div className='pt-20 ml-[10vw] font-["Neue_Montreal"] font-medium text-[1.1vw] '>
                     <ul>
                         <th> S:</th>
-                        <li>
+                        <li className='mt-10'>
                        <a href="">Instagram</a> 
                         </li>
                         <li>
@@ -46,9 +46,17 @@ function About() {
             </div>
             
         </div>
-        <div className='w-full mt-20 border-t-[1px] border-[#b6d455]'></div>
-        <div className='w-full h-[40vh] mt-5 flex'>
+        <div className='w-full flex gap-5 mt-20 border-t-[1px] pt-10 border-[#b6d455]'>
+            <div className='w-1/2 px-3'>
+                <h1 className='text-7xl'>Our approach: </h1>
+                <button className='flex uppercase gap-10 items-center px-10 py-6 bg-zinc-900 mt-10 text-white rounded-full hover:bg-zinc-800'>Read More 
+                    <div className='w-3 h-3 bg-zinc-100 rounded-full'>
+                    </div>
+                </button>
+            </div>
+            <div className='w-1/2 h-[70vh] rounded-3xl mr-16  bg-[#81963c] '>
 
+            </div>
         </div>
     </div>
   )
